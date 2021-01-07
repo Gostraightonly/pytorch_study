3815608d1315f1a9693fba29cbc1d667a0d6d7bb
Gostraightonly <76559328+Gostraightonly@users.noreply.github.com>
2021-01-07 21:14:23 +0900

    Add files via upload

---
+++ 8-1. pytorch_softmax_MNIST.py
@@ -0,0 +1,107 @@
+# https://github.com/pytorch/examples/blob/master/mnist/main.py
+from __future__ import print_function #python2에서 python3의 문법인 print()를 사용하기 위함(나의 경우 해당없음)
+from torch import nn, optim, cuda
+from torch.utils import data
+from torchvision import datasets, transforms
+import torch.nn.functional as F
+import time
+
+# Training settings
+batch_size = 64
+device = 'cuda' if cuda.is_available() else 'cpu' #cuda가 있으면 cuda로 없으면 cpu로
+print(f'Training MNIST Model on {device}\n{"=" * 44}')
+
+# MNIST Dataset
+
+train_dataset = datasets.MNIST(root='./mnist_data/',
+                               train=True,
+                               transform=transforms.ToTensor(),
+                               download=True)
+
+test_dataset = datasets.MNIST(root='./mnist_data/',
+                              train=False,
+                              transform=transforms.ToTensor())
+
+# Data Loader (Input Pipeline)
+#Train에만 쓰이는 dataloader
+train_loader = data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+#Test에만 쓰이는 dataloader
+test_loader = data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.l1 = nn.Linear(784, 520) #input = 784
+        self.l2 = nn.Linear(520, 320)
+        self.l3 = nn.Linear(320, 240)
+        self.l4 = nn.Linear(240, 120)
+        self.l5 = nn.Linear(120, 10) #output = 10(0~9 사이의 정수)
+
+    def forward(self, x):
+        x = x.view(-1, 784)  # Flatten the data (n, 1, 28, 28)-> (n, 784)
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = F.relu(self.l3(x))
+        x = F.relu(self.l4(x))
+        return self.l5(x)
+
+
+model = Net()
+model.to(device) #model에서 사용하는 input Tensor들은 input.to(device)를 호출해야함.
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} | Batch Status: {}/{} ({:.0f}%) | Loss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test():
+    model.eval() #eval mode 선언, 하지 않으면 추론 결과가 일관성 없게 출력됨
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        # sum up batch loss
+        test_loss += criterion(output, target).item()
+        # get the index of the max
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print(f'===========================\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} '
+          f'({100. * correct / len(test_loader.dataset):.0f}%)')
+
+
+if __name__ == '__main__':
+    since = time.time()
+    for epoch in range(1, 10):
+        epoch_start = time.time()
+        train(epoch)
+        m, s = divmod(time.time() - epoch_start, 60)
+        print(f'Training time: {m:.0f}m {s:.0f}s')
+        test()
+        m, s = divmod(time.time() - epoch_start, 60)
+        print(f'Testing time: {m:.0f}m {s:.0f}s')
+
+    m, s = divmod(time.time() - since, 60)
+    print(f'Total Time: {m:.0f}m {s:.0f}s\nModel was trained on {device}!')
